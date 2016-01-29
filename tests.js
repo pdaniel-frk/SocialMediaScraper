94cd8f5a3e3ab44b4b83ae6d84c6808747df895d
Dash-Wedergren <dashwedergren@gmail.com>
2016-01-28 19:06:55 -0600

    improved test.js and added offending tweets request

--- tests.js
+++ tests.js
@@ -2,21 +2,38 @@ var assert = require("assert");
 var request = require("request");
 
 request("http://localhost:9998/users?id=2", function(error, response, body) {
-	body = JSON.parse(body)
-	console.log(body);
+	if(body){
+		body = JSON.parse(body)
+		console.log("Users:");
+		console.log(body);
+		console.log("\n");
+	}
 });
 
 
 request("http://localhost:9998/kids?twit_id=20293902", function(error, response, body){
 	if(body){
 		body = JSON.parse(body);
+		console.log("Kids:");
 		console.log(body);
+		console.log("\n");
 	}
 });
 
-request("http://localhost:9998/words?id=1", function(error, response,body){
+request("http://localhost:9998/words?id=1", function(error, response ,body){
 	if(body){
 		body = JSON.parse(body);
+		console.log("Words:")
 		console.log(body);
+		console.log("\n");
+	}
+});
+
+request("http://localhost:9998/offending_tweets?twit_id=20293902", function(error, response, body){
+	if(body){
+		body = JSON.parse(body);
+		console.log("Offending_Tweets:");
+		console.log(body);
+		console.log("\n");
 	}
 });
