b78b819f0e293b7be6bff69a38d0b2ca0da21e0d
Dash-Wedergren <dashwedergren@gmail.com>
2016-02-07 09:21:00 -0600

    Update

--- src/main/java/com/gallup/gethip/Main.java
+++ src/main/java/com/gallup/gethip/Main.java
@@ -8,7 +8,6 @@ import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
 import com.sun.jersey.api.core.PackagesResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import org.glassfish.grizzly.http.server.HttpServer;
-
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
 import java.net.URI;
@@ -17,7 +16,7 @@ import java.sql.*;
 public class Main {
 	
 	private static void createDatabaseConnection(){
-		String databaseUrl = "jdbc:";
+		String databaseUrl = "";
 		ConnectionSource connectionSource;
 		try {
 			connectionSource = new JdbcConnectionSource(databaseUrl);
